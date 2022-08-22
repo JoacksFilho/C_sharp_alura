a8aef444af24e351fedf434358a29109f37cf7c2
Joacks Filho <joackslemos@hotmail.com>
2022-08-22 19:42:58 -0300

    updates

--- problema_logico/Programa.cs
+++ problema_logico/Programa.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿//Se listarmos todos os números naturais abaixo de 10 que são múltiplos de 3 ou 5, obtemos 3, 5, 6 e 9. A soma desses múltiplos é 23.
+//Elabore um método que retorne a soma de todos os múltiplos de 3 ou 5 abaixo do número passado. Além disso, se o número for negativo, retorne 0.
+//Nota: Se o número for múltiplo de 3 e 5, conte-o apenas uma vez.
 
-class Desafio
+
+
+using System;
+
+class Programa
 {
     static void Main(string[] arg)
     {
@@ -14,7 +20,7 @@ class Desafio
        {    
             if(numero < 0)
             {
-                Console.WriteLine(0);
+                Console.WriteLine(0);// como que usa o return no c#?
             }
             if (i % 3 == 0 || i % 5 == 0)
            {
